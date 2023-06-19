d411c6a004b81ddcef5147ea6a75fc6657817f95
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-06-19 21:25:47 +0530

    base wp struct

--- birthday-email-sender/birthday-email-sender.php
+++ birthday-email-sender/birthday-email-sender.php
@@ -1,21 +1,24 @@
 <?php
-/**
- * Plugin Name: Birthday Email Sender
- * Description: This plugin sends an email to the user on his birthday.
- * Version: 0.0.1-dev (18-06-2023)
- * Author: Shubham Kumar Bansal
- * Author URI: https://shubkb.me
- */
-
- if(!defined('ABSPATH')){
-    exit;
- }
- function birthday_email_sender_activation(){
-    
- }
- function birthday_email_sender_deactivation(){
-     
- }
- register_activation_hook(__FILE__,'birthday_email_sender_activation');
- register_deactivation_hook(__FILE__,'birthday_email_sender_deactivation');
+	/**
+	* Plugin Name: Birthday Email Sender
+	* Description: This plugin sends an email to the user on his birthday.
+	* Version: 0.0.1-dev (18-06-2023)
+	* Author: Shubham Kumar Bansal
+	* Author URI: https://shubkb.me
+	*/	
+	if( !defined('ABSPATH' )){
+		exit;
+	}
+	function birthday_email_sender_activation(){
+		
+	}
+	function birthday_email_sender_deactivation(){
+		
+	}
+	function birthday_email_sender_list(){
+		return '<h1> Birthday Email Sender </h1>';
+	}
+	register_activation_hook(__FILE__,'birthday_email_sender_activation');
+	register_deactivation_hook(__FILE__,'birthday_email_sender_deactivation');
+	addshortcode('birthday_email_sender_list','birthday_email_sender_list');
 ?>
