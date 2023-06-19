207321cd35831861874de04825839f3edad6ff86
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-06-19 23:34:06 +0530

    learn database for wp

--- birthday-email-sender/birthday-email-sender.php
+++ birthday-email-sender/birthday-email-sender.php
@@ -5,20 +5,26 @@
 	* Version: 0.0.1-dev (18-06-2023)
 	* Author: Shubham Kumar Bansal
 	* Author URI: https://shubkb.me
-	*/	
-	if( !defined('ABSPATH' )){
+	*/
+	if(!defined('ABSPATH')){
 		exit;
 	}
 	function birthday_email_sender_activation(){
-		
+		global $wpdb, $table_prefix;
+		$table_name = $table_prefix.'_birthday_extra';
+		$q = "CREATE TABLE IF NOT EXISTS `$table_name` (
+			`ID` INT NOT NULL AUTO_INCREMENT , 
+			`email` VARCHAR(50) NOT NULL , 
+			`name` VARCHAR(50) NOT NULL , 
+			`dob` DATE NOT NULL , 
+			`msgtype` INT NOT NULL , 
+			`msg` TEXT NOT NULL , 
+			PRIMARY KEY (`ID`)) ENGINE = InnoDB;";
+		$wpdb->query($q);
 	}
 	function birthday_email_sender_deactivation(){
-		
-	}
-	function birthday_email_sender_list(){
-		return '<h1> Birthday Email Sender </h1>';
+
 	}
 	register_activation_hook(__FILE__,'birthday_email_sender_activation');
 	register_deactivation_hook(__FILE__,'birthday_email_sender_deactivation');
-	addshortcode('birthday_email_sender_list','birthday_email_sender_list');
 ?>
--- birthday-email-sender/uninstall.php
+++ birthday-email-sender/uninstall.php
@@ -1,5 +1,6 @@
-<?php
-    if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-        die;
-    }
+<?php 
+	global $wpdb, $table_prefix;
+	$table_name = $table_prefix.'_birthday_extra';
+	$q = "DROP TABLE IF EXISTS `$table_name`;";
+	$wpdb->query($q);
 ?>
